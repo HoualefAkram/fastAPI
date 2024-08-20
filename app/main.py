84d6a6b945945a97720a5dc8401cdf7bdf02c552
HoualefAkram <akram1232004@gmail.com>
2024-08-20 21:34:53 +0100

    hello world

--- app/main.py
+++ app/main.py
@@ -9,6 +9,12 @@ from .routers import post, user, auth, vote
 
 app = FastAPI()
 
+
+@app.get("/")
+def hello_world():
+    return {"message": "Hello World!"}
+
+
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
